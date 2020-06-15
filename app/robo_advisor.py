2d9d2370073686e8e6a069b3ac80cb3e9afe2cc6
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-06-15 00:50:35 -0400

    get latest refreshed date

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -11,8 +11,16 @@ response = requests.get(request_url)
 #print(response.text)
 
 parsed_response = json.loads(response.text)
+#parsed_response.keys
+#parsed_response["Meta Data"]
+#parsed_response["Meta Data"].keys
+#parsed_response["Meta Data"]["3. Last Refreshed"]
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-breakpoint()
+
+
+
+#breakpoint()
 
 
 
@@ -23,9 +31,9 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: 2018-02-20 02:00pm") #datetime module
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
