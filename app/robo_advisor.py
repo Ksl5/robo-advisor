ead39cdcc21b86e1c3aa0e04398cea2fe9f313c6
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-06-15 23:04:37 -0400

    write dexpected headers to csv file

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,6 +4,7 @@ import json
 import datetime
 import os
 import csv
+
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
@@ -50,13 +51,22 @@ recent_low = min(low_prices)
 #csv_file_path = "data/prices.csv" # stock prices filepath
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
+csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
 with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+
     writer.writeheader() # uses fieldnames set above
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
+           
+    writer.writerow({
+        "timestamp": "TODO",
+        "open":"TODO", 
+        "high":"TODO",
+        "low":"TODO",
+        "close":"TODO",
+        "volume":"TODO"
+    })
+    
 
 print("-------------------------")
 print("SELECTED SYMBOL: " + selected_symbol)
