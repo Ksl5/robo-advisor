3063bc98beaf259e03c19a253417092abff5c0e6
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-06-16 00:02:53 -0400

    loading environmenmt var from dotenv

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,6 +4,8 @@ import json
 import datetime
 import os
 import csv
+from dotenv import load_dotenv
+load_dotenv()
 
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
@@ -11,7 +13,12 @@ def to_usd(my_price):
 
 
 # Info inputs
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+#symbol = parsed_response["Meta Data"]["2. Symbol"]
+
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+symbol = "MSFT"
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 response = requests.get(request_url)
 #print(type(response)) #> <class 'requests.models.Response'>
 #print(response.status_code) #> 200
@@ -39,7 +46,7 @@ for date in dates:
     low_prices.append(float(low_price))
 
 
-selected_symbol = parsed_response["Meta Data"]["2. Symbol"]
+
 latest_close = tsd [latest_day]["4. close"]
 recent_high = max(high_prices)
 #recent_high = tsd[latest_day]["2. high"]
@@ -70,7 +77,7 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
 
 
 print("-------------------------")
-print("SELECTED SYMBOL: " + selected_symbol)
+print("SELECTED SYMBOL: " + symbol)
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA")
 print("REQUEST AT: " + now.strftime('%Y-%m-%d %H:%M:%S %p'))
